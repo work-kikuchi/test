bd84d2f8e0b0fe0a6b61d06c1a1703c7afea60c2
tkikuchi <tkikuchi@landscape.co.jp>
2023-04-25 14:48:43 +0900

    テスト

---
+++ wp-tutorial-https-github-com-ionic-jp-starters-v-4-angular-blank-git/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'wp-tutorial https://github.com/ionic-jp/starters-v4-angular-blank.git',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
